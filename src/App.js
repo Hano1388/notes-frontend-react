c87e19e9e4e867f6b5e6b31531ccde588ed69e9e
Hano <hinsul3113@gmail.com>
2018-01-09 12:20:57 -0800

    created a component to add, remove, view notes

--- src/App.js
+++ src/App.js
@@ -7,33 +7,69 @@ class App extends Component {
   constructor() {
     super();
     this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
+    this.details = this.details.bind(this);
     this.state = {
       notes: [],
       title: 'Simple Note Application',
       body: 'note body',
-      date: '2018-01-01'
+      date: '2018-01-01',
+      counter: 0
     }
   }
 
+  // handling add note
   addNote(event) {
-    debugger
+    // debugger
     event.preventDefault();
     let title = this.refs.title.value;
     let body = this.refs.body.value;
     let date = this.refs.date.value;
+    let counter = this.state.counter;
 
     let note = {
       title,
       body,
-      date
+      date,
+      counter
     };
+    counter += 1;
     let notes = this.state.notes;
 
     notes.push(note);
 
     this.setState({
+      notes: notes,
+      counter: counter
+    });
+    this.refs.noteForm.reset();
+  }
+
+  // handling removing note
+  removeNote(index) {
+    // console.log(index);
+    let notes = this.state.notes;
+
+    let note = notes.find(note => {
+      return note.counter === index;
+    });
+    // console.log(note);
+    notes.splice(note, 1);
+    this.setState({
       notes: notes
-    })
+    });
+  }
+
+  // handling note details
+  details(index) {
+    let notes = this.state.notes;
+
+    let note = notes.find(note => {
+      return note.counter === index;
+    });
+
+    console.log('You are clicking on ');
+    console.log(note);
   }
 
   render() {
@@ -42,15 +78,22 @@ class App extends Component {
     return (
       <div className="App">
         <h1>{title}</h1>
-        <form>
+        <form ref="noteForm">
           <input type="text" ref="title" placeholder="note title" />
           <input type="text" ref="body" placeholder="what is your note about" />
           <input type="date" ref="date" placeholder="2018-01-01" />
           <button onClick={this.addNote}>Add Note</button>
         </form>
-        <pre>
-          {JSON.stringify(notes)}
-        </pre>
+        <ul>
+          {notes.map((note =>
+            <li key={note.counter}>
+              {note.title} -
+              {note.body} -
+              {note.date} -
+              <button onClick={this.removeNote.bind(null, note.counter)}>Remove Note</button>
+              <button onClick={this.details.bind(null, note.counter)}>View Details</button>
+          </li>))}
+        </ul>
       </div>
     );
   }
